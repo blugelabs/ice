b4acb45dc3cfef69bf028148eac29937519875d5
yanghengfei <hengfei.yang@gmail.com>
2022-05-02 10:03:14 +0800

    feat: packed numeric of posting list

--- write.go
+++ write.go
@@ -90,7 +90,11 @@ func writePostings(postings *roaring.Bitmap, tfEncoder, locEncoder *chunkedIntCo
 		return 0, err
 	}
 
-	n = binary.PutUvarint(bufMaxVarintLen64, locOffset)
+	if locOffset > 0 && tfOffset > 0 {
+		n = binary.PutUvarint(bufMaxVarintLen64, locOffset-tfOffset)
+	} else {
+		n = binary.PutUvarint(bufMaxVarintLen64, locOffset)
+	}
 	_, err = w.Write(bufMaxVarintLen64[:n])
 	if err != nil {
 		return 0, err
